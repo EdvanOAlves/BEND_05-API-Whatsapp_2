242f6bd1b8ab0124702c21e06c142d3b1605aabc
EDVAN ALVES DE OLIVEIRA <25132698@CFP127ALUNO.LOCAL>
2025-09-24 11:12:28 -0300

    feat: fully implements getUserProfile

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -20,42 +20,80 @@ const MESSAGE_ERRO = {
     development: 'Edvan Alves de Oliveira'
 };
 
+const MESSAGE_NOT_FOUND = {
+    status: false,
+    status_code: 404,
+    development: 'Edvan Alves de Oliveira'
+}
+
 //Importando dados
 const dados = require('./contatos.js')
+const users = dados.contatos['whats-users'];
 
-// Retorna tudo de todos, Mark Zuckerberg quem pediu
-const getAllData = function (){
+// Retorna tudo de todos
+const getAllData = function () {
     let message = {
         status: true,
         status_code: 200,
         development: 'Edvan Alves de Oliveira',
-        users:[]
+        users: []
     }
 
-    dados.contatos['whats-users'].forEach(function (usuario){
+    users.forEach(function (usuario) {
         message.users.push(usuario);
     })
 
-    if (message.users.length){
+    if (message.users.length) {
         return message;
     }
     else
         return MESSAGE_ERRO;
 }
 
-console.log(getAllData());
+//Retorna um usuário com o número de telefone, função reutilizável
+const getUserWithNumber = function(phoneNumber){
+    const user = users.find(usuario => usuario.number == phoneNumber);
+    return user;
+}
 
 // Retorna dados do usuário
-const getUserProfile = function(/*Usuário*/){}
+const getUserProfile = function (phoneNumber) {
+    let message = {
+        status: true,
+        status_code: 200,
+        development: 'Edvan Alves de Oliveira',
+        user_profile: {}
+    }
+
+    const user = getUserWithNumber(phoneNumber);
+    if (user){
+        message.user_profile.id = user.id;
+        message.user_profile.phone_number = user.number;
+        message.user_profile.name = user.account;
+        message.user_profile.nickname = user.nickname;
+        message.user_profile.registration_date = user['created-since'].start;
+        message.user_profile.termination_date = user['created-since'].end;
+        message.user_profile.profile_image = user['profile-image'];
+        message.user_profile.background = user.background;
+    }else
+        return MESSAGE_NOT_FOUND;   // 404, Caso não tenha sido encontrado aquele número
+
+    if (message.user_profile)
+        return message  // 200 deu tudo certo
+    else
+        return MESSAGE_ERRO // 500 Alguma outra coisa deu errado
+        //Desse jeito acho que nunca dá esse, só 404
+
+}
 
 // Retorna dados dos contatos associados com um usuário
-const getContactList = function(/*Usuário*/){}
+const getContactList = function (/*Usuário*/) { }
 
 // Retorna todas as conversas de um usuário
-const getUserMessages = function(/*Usuário*/){}
+const getUserMessages = function (/*Usuário*/) { }
 
 // Retorna todas as mensagens de um usuário com um contato
-const getChats = function(/*Usuário,*/ /*Contato*/){}
+const getChats = function (/*Usuário,*/ /*Contato*/) { }
 
 // Retorna mensagens com um filtro usando uma palavra chave
-const searchWithKeyWord = function(/*Palavra-chave*/){}
+const searchWithKeyWord = function (/*Palavra-chave*/) { }
