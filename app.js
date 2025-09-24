0586788204f7c464047f0633e038505fd708e43f
EDVAN ALVES DE OLIVEIRA <25132698@CFP127ALUNO.LOCAL>
2025-09-24 08:41:00 -0300

    chore: init

---
+++ app.js
@@ -0,0 +1,26 @@
+/* *********************************************************************
+* Objetivo: endPoints referente a API de estados e cidades
+* Data: 15/09/2025
+* Autor: Edvan
+* Versão: 1.0.0
+*
+* Observações: Instalação do Express, Cors, Body-Parser
+
+* **********************************************************************/
+
+/*********************************************************************** 
+ * COMANDOS UTILIZADOS
+ * 
+ * Instalação do Express, Cors, Body-Parser:
+ *  npm install express      --save
+ *  npm install cors         --save
+ *  npm install body-parser  --save
+ * 
+ * 
+ * Instalação local quando baixar esse projeto:
+ *  npm i
+ * 
+ /**********************************************************************
+ //BIBLIOTECAS UTILIZADAS
+ * 
+ **********************************************************************/
